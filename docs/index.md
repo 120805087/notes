79606ada56856492e4d179868b310f61c8bc2a1d
fengnn <fengnn@kmelearning.com>
2024-02-23 14:23:12 +0800

    build: init project

---
+++ docs/index.md
@@ -0,0 +1,15 @@
+---
+pageType: home
+
+hero:
+  name: Felity Notes
+  text: 没有阳光的温度，鲜花不会绽放
+  tagline: 随缘笔记, 仅供个人学习使用
+  actions:
+    - theme: brand
+      text: 笔记
+      link: /note/
+  image:
+    src: /felity-icon.png
+    alt: Blog Logo
+---
---
+++ rspress.config.ts
@@ -0,0 +1,29 @@
+import * as path from "path";
+import { defineConfig } from "rspress/config";
+
+export default defineConfig({
+  root: path.join(__dirname, "docs"),
+  base: "/notes/",
+  title: "Felity Notes",
+  description: "随缘笔记，仅供个人学习使用",
+  icon: "/felity-icon.png",
+  logo: {
+    light: "/felity-light-logo.png",
+    dark: "/felity-dark-logo.png",
+  },
+  themeConfig: {
+    socialLinks: [
+      {
+        icon: "github",
+        mode: "link",
+        content: "https://github.com/120805087/notes",
+      },
+    ],
+    searchPlaceholderText: "搜索",
+    prevPageText: "上一页",
+    nextPageText: "下一页",
+    footer: {
+      message: "© 2024 Felity. All Rights Reserved. ",
+    },
+  },
+});
