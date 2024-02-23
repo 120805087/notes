ff94505c5900215f24617a5c87c5f863b5ea0662
fengnn <fengnn@kmelearning.com>
2024-02-23 16:45:20 +0800

    ci: 集成 @arco-design

---
+++ src/pages/nav/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@arco-design/web-react";
+import "@arco-design/web-react/dist/css/arco.css";
+
+export default function Nav() {
+  return (
+    <div>
+      <Button type="primary">Nav</Button>
+    </div>
+  );
+}
