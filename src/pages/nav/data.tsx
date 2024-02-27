426083c36bf4124309f29a7e3a2ccdefea4a4651
fengnn <fengnn@kmelearning.com>
2024-02-27 12:53:56 +0800

    feat: add nav

--- src/pages/nav/data.tsx
+++ src/pages/nav/data.tsx
@@ -19,6 +19,21 @@ export const data: {
         name: "FramerMotion",
         link: "https://www.framer.com/motion/",
       },
+      {
+        icon: "/nav/next.png",
+        name: "Next",
+        link: "https://nextjs.org/docs/app/api-reference/edge#stream-apis",
+      },
+      {
+        icon: "/nav/react-hook-form.png",
+        name: "ReactHookForm",
+        link: "https://react-hook-form.com/get-started",
+      },
+      {
+        icon: "/nav/tanstack-query.png",
+        name: "TanstackQuery",
+        link: "https://tanstack.com/query/latest/docs/framework/react/overview",
+      },
     ],
   },
   {
@@ -48,6 +63,31 @@ export const data: {
         icon: "/nav/ant-design.png",
         name: "AntDesign",
         link: "https://ant-design.antgroup.com/index-cn",
+      },
+      {
+        icon: "/nav/radix.png",
+        name: "Radix",
+        link: "https://www.radix-ui.com/themes/docs/overview/getting-started",
+      },
+    ],
+  },
+  {
+    groupName: "Tools",
+    items: [
+      {
+        icon: "/nav/zod.png",
+        name: "Zod",
+        link: "https://zod.dev/?id=basic-usage",
+      },
+    ],
+  },
+  {
+    groupName: "Database",
+    items: [
+      {
+        icon: "/nav/prisma.png",
+        name: "Prisma",
+        link: "https://www.prisma.io/docs/orm/overview/introduction/what-is-prisma",
       },
     ],
   },
